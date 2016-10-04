8a08474027cda0f3f45e7b973fd9feb113d2f770
John Diamond <johndiamond@gmx.co.uk>
2016-10-04 18:35:25 +0200

    demo json parsing fixes

--- Example/DeepstreamIO/ViewController.swift
+++ Example/DeepstreamIO/ViewController.swift
@@ -11,14 +11,14 @@ import UIKit
 typealias RecordSubscriptionCallbackHandler = ((recordName: String, path: String, data: JsonElement) -> Void)
 
 class RecordSubscriptionCallback : NSObject, RecordPathChangedCallback {
-    
+
     private var handler : RecordSubscriptionCallbackHandler!
-    
+
     func onRecordPathChanged(recordName: String!, path: String!, data: JsonElement!) {
         print("\(recordName):\(path) = \(data)")
         self.handler(recordName: recordName, path: path, data: data)
     }
-    
+
     init(handler: RecordSubscriptionCallbackHandler) {
         self.handler = handler
     }
@@ -42,31 +42,32 @@ extension DeepstreamClient
 }
 
 final class ViewController: UIViewController {
-    
+
     // MARK: - UI Outlets
-    
+
     @IBOutlet weak var textField: UITextField!
-    
+
     // Deepstream
-    
+
         var client : DeepstreamClient?
         var record : Record?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupDeepstream()
     }
-    
+
     func setupDeepstream() {
         let qualityOfServiceClass = QOS_CLASS_BACKGROUND
         let backgroundQueue = dispatch_get_global_queue(qualityOfServiceClass, 0)
         dispatch_async(backgroundQueue, {
-            
+
             // Setup Deepstream.io client 
             // NOTE: REPLACE HOST
-            self.client = DeepstreamClient("138.68.154.77:6021")
+
+            self.client = DeepstreamClient("dashboard-ds-dev.deepstreamhub.com:6021")
             self.client?.setRuntimeErrorHandler(RuntimeErrorHandler())
-            
+
             sleep(5)
             guard let client = self.client else {
                 print("Error: Unable to init client")
@@ -92,23 +93,27 @@ final class ViewController: UIViewController {
             self.record = record
 
             // Init UI against latest record state
-            let currentRecordText = client.record?.getRecord("firstname").get().getAsString()
+            let currentRecordText = client.record?.getRecord("some-name").get("firstname")
             dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                self.textField.text = currentRecordText
+                if(( currentRecordText?.getAsJsonPrimitive().isString() ) != nil) {
+                    self.textField.text = currentRecordText?.getAsString()
+                }
             })
 
             // Subscribe to updates
             let callback = RecordSubscriptionCallback(handler: { (recordName, path, data) -> Void in
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    let recordText = data.getAsString()
-                    self.textField.text = recordText
+                    if( data.getAsJsonPrimitive().isString() ) {
+                        let recordText = data.getAsString()
+                        self.textField.text = recordText
+                    }
                 })
             })
-            
+
             record.subscribe("firstname", recordPathChangedCallback: callback)
         })
     }
-    
+
     @IBAction func editingChanged(sender: UITextField) {
         if let record = self.record {
             record.set("firstname", value: sender.text)
