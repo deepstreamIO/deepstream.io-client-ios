cb06b7d1ff1cd1426e26f204d9e1d240701bb650
Akram Hussein <akramhussein@gmail.com>
2016-12-17 14:09:50 +0000

    Init stub for Publisher

---
+++ TestApp/TestApp/TestApp/Publisher.swift
@@ -0,0 +1,15 @@
+//
+//  Publisher.swift
+//  TestApp
+//
+//  Created by Akram Hussein on 17/12/2016.
+//
+//
+
+import Foundation
+
+final public class Publisher {
+    init() {
+        print("Created Publisher")
+    }
+}
--- TestApp/TestApp/TestApp/main.swift
+++ TestApp/TestApp/TestApp/main.swift
@@ -8,5 +8,5 @@
 
 import Foundation
 
-print("Hello, World!")
+let publisher = Publisher()
 
