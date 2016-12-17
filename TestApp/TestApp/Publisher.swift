c9776035db2eb389c404136db4c88ae77f0bb1c8
Akram Hussein <akramhussein@gmail.com>
2016-12-17 14:49:52 +0000

    Added cocoapod and bridging header

--- TestApp/TestApp/Publisher.swift
+++ TestApp/TestApp/Publisher.swift
@@ -3,12 +3,15 @@
 //  TestApp
 //
 //  Created by Akram Hussein on 17/12/2016.
-//
+//  Copyright (c) 2016 deepstreamHub GmbH. All rights reserved.
 //
 
 import Foundation
 
 final public class Publisher {
+    
+    private var client : DeepstreamClient?
+    
     init() {
         print("Created Publisher")
     }
