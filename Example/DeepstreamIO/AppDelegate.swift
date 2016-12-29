c0575853443e31255a328ec54eddb43e03c3b028
Akram Hussein <akramhussein@gmail.com>
2016-12-29 13:28:57 +0000

    Updated AppDelegate

--- Example/DeepstreamIO/AppDelegate.swift
+++ Example/DeepstreamIO/AppDelegate.swift
@@ -13,8 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    private func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        
+        
         return true
     }
 }
